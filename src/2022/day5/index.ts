598c74d8712ea78d5aae2a051a867759c2112d24
Macke Hansson <markus.hansson@cgi.com>
2022-12-06 09:21:47 +0100

    Day 5

--- src/2022/day5/index.ts
+++ src/2022/day5/index.ts
@@ -1,97 +1,76 @@
-const testInput = [
-    '    [D]    ',
-    '[N] [C]    ',
-    '[Z] [M] [P]',
-    ' 1   2   3 ',
-    '',
-    'move 1 from 2 to 1',
-    'move 3 from 1 to 3',
-    'move 2 from 2 to 1',
-    'move 1 from 1 to 2',
-];
-
+import { getInput } from '../../utils/getInput'
+import produce from 'immer'
+const inputInstructions = getInput(__dirname + '/input.txt')
 interface MoveModel {
-    move: number;
-    from: number;
-    to: number;
+    move: number
+    from: number
+    to: number
 }
 
-function formatInput(input: string[], stacks = [], instructions = [], columns = '') {
-    input.forEach((i) => {
-        if (i.charAt(0) === 'm') instructions.push(i);
-        if (i.includes('[')) stacks.push(i);
-        if (/^\d$/.test(i.charAt(1))) columns = i;
-    });
+const inputStacks = {
+    1: ['Z', 'J', 'G'],
+    2: ['Q', 'L', 'R', 'P', 'W', 'F', 'V', 'C'],
+    3: ['F', 'P', 'M', 'C', 'L', 'G', 'R'],
+    4: ['L', 'F', 'B', 'W', 'P', 'H', 'M'],
+    5: ['G', 'C', 'F', 'S', 'V', 'Q'],
+    6: ['W', 'H', 'J', 'Z', 'M', 'Q', 'T', 'L'],
+    7: ['H', 'F', 'S', 'B', 'V'],
+    8: ['F', 'J', 'Z', 'S'],
+    9: ['M', 'C', 'D', 'P', 'F', 'H', 'B', 'T'],
+}
 
-    const formattedColumns = Math.max(
-        ...columns
-            .split(' ')
-            .filter((i) => i.length > 0)
-            .map(Number),
-    );
+const testInputStacks = {
+    1: ['Z', 'N'],
+    2: ['M', 'C', 'D'],
+    3: ['P'],
+}
 
-    const formattedInstructions = instructions.reduce<MoveModel[]>((acc, curr) => {
-        const draft = curr.replace(/\s/g, '').match(/(\d+)/gm).map(Number);
+const testInstructions = ['move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
+
+function formatInstructions(rawInstructions: string[]) {
+    return rawInstructions.reduce<MoveModel[]>((acc, curr) => {
+        const draft = curr.replace(/\s/g, '').match(/(\d+)/gm).map(Number)
         acc.push({
             move: draft[0],
             from: draft[1],
             to: draft[2],
-        });
-        return acc;
-    }, []);
+        })
+        return acc
+    }, [])
+}
 
-    const formatedStacks = {};
-
-    stacks.forEach((i) => {
-        const row = i.match(/.{1,4}/g).map((i) => i.replace(/\s/g, ''));
-        let cols = 0;
-        while (cols < formattedColumns) {
-            const fsNo = cols + 1;
-            if (!formatedStacks[fsNo]) {
-                formatedStacks[fsNo] = [row[cols]];
+function moveStacks(stacks: Record<number, string[]>, instructions: MoveModel[], model9001 = false) {
+    return produce(stacks, (nextState) => {
+        instructions.forEach((i) => {
+            const source = nextState[i.from]
+            const target = nextState[i.to]
+            const amount = i.move
+            const payload = source.splice(Math.abs(amount) * -1, amount)
+            if (model9001) {
+                target.push(...payload)
             } else {
-                formatedStacks[fsNo].push(row[cols]);
+                target.push(...payload.reverse())
             }
+        })
+        return nextState
+    })
+}
 
-            cols++;
+function getTopBoxes(stack: Record<number, string[]>) {
+    const result = []
+    for (const box in stack) {
+        const stackBoxes = stack[box]
+        if (stackBoxes.length >= 1) {
+            const topBox = stackBoxes.at(-1)
+
+            result.push(topBox)
         }
-    });
-
-    const formattedStacks: string[][] = [];
-    for (const o in formatedStacks) {
-        const boxes = formatedStacks[o]
-            .filter((i) => i.length > 0)
-            .reverse()
-            .map((i) => i.replace('[', '').replace(']', ''));
-        formattedStacks.push(boxes);
     }
-
-    return { formattedStacks, formattedColumns, formattedInstructions };
+    return result.join('')
 }
 
-function moveBoxesInStack(input: string[][], instruction: MoveModel) {
-    const source = input[instruction.from - 1];
-    const target = input[instruction.to - 1];
-    const package = source.pop();
-    target.push(package);
-    return input;
-}
-
-function puzzle1(input: string[]) {
-    const formattedInput = formatInput(input);
-
-    let data = formattedInput.formattedStacks;
-    formattedInput.formattedInstructions.forEach((i) => {
-        data = moveBoxesInStack(formattedInput.formattedStacks, i);
-    });
-
-    // console.log(
-    //     moveBoxesInStack(formattedInput.formattedStacks, {
-    //         move: 1,
-    //         from: 2,
-    //         to: 1,
-    //     }),
-    // );
-}
-
-puzzle1(testInput);
+const instructions = formatInstructions(inputInstructions)
+const movedBoxes = moveStacks(inputStacks, instructions)
+const movedBoxes9001 = moveStacks(inputStacks, instructions, true)
+console.log(getTopBoxes(movedBoxes))
+console.log(getTopBoxes(movedBoxes9001))
