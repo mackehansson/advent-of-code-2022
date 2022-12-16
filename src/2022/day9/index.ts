dcdd76b5f34fd7f9184de12152562c3be8b8df1d
Macke Hansson <markus.hansson@cgi.com>
2022-12-16 16:04:10 +0100

    head movements day 9

--- src/2022/day9/index.ts
+++ src/2022/day9/index.ts
@@ -115,13 +115,13 @@ R 4(4)      [4,0]      [3,0]    xD: 2, yD: 0    3,0
 
                                 Y+
 U 4(1)      [4,1]      [3,0]    xD: 1, yD: 1    3,0
-U 4(2)      [4,2]      [4,1]    xD: 1, yD: 2    Diagonalt   X + 1, Y + 1
+U 4(2)      [4,2]      [4,1]    xD: 1, yD: 2    Diagonalt   X + 1, Y + 1    Blir T samma som H va steget innan? Ja.
 U 4(3)      [4,3]      [4,2]    xD: 0, yD: 2    
 U 4(4)      [4,4]      [4,3]    xD: 0, yD: 2    
 
                                 X-
 L 3(1)      [3,4]      [4,3]    xD: 1, yD: 1
-L 3(2)      [2,4]      [3,4]    xD: 2, yD: 1    Diagonalt   X - 1, Y + 1
+L 3(2)      [2,4]      [3,4]    xD: 2, yD: 1    Diagonalt   X - 1, Y + 1. Blir T samma som H va steget innan? Ja
 L 3(3)      [1,4]      [2,4]    xD: 2, yD: 0
 
                                 Y-
@@ -130,7 +130,7 @@ D 1(1)      [1,3]      [2,4]    xD: 1, yD: 1
                                 X+
 R 4(1)      [2,3]      [2,4]    xD: 0, yD: 1
 R 4(2)      [3,3]      [2,4]    xD: 1, yD: 1
-R 4(3)      [4,3]      [3,3]    xD: 2, yD: 1    Diagonalt
+R 4(3)      [4,3]      [3,3]    xD: 2, yD: 1    Diagonalt. Blir T samma som H va steget innan? Ja
 R 4(4)      [5,3]      [4,3]    xD: 2, yD: 0
 
                                 Y-
@@ -149,37 +149,222 @@ R 2(2)      [2,2]      [1,2]    xD: 1, yD: 0
 
 
 
+[0,0]     [0,0]
 
-[0,0]
-[1,0]
-[2,0]
-[3,0]
-[4,0]
-[4,1]
-[4,2]
-[4,3]
-[4,4]
-[3,4]
-[2,4]
-[1,4]
-[1,3]
-[2,3]
-[3,3]
-[4,3]
-[5,3]
-[5,2]
-[4,2]
-[3,2]
-[2,2]
-[1,2]
-[0,2]
-[1,2]
-[2,2]
+[1,0]     [0,0]
+[2,0]     [1,0]
+[3,0]     [2,0]
+[4,0]     [3,0]
+[4,1]     [3,0]
+[4,2]     [4,1]
+[4,3]     [4,2]
+[4,4]     [4,3]
+[3,4]     [4,3]
+[2,4]     [3,4]
+[1,4]     [2,4]
+[1,3]     [2,4]
+[2,3]     [2,4]
+[3,3]     [2,4]
+[4,3]     [3,3]
+[5,3]     [4,3]
+[5,2]     [4,3]
+[4,2]     [4,3]
+[3,2]     [4,3]
+[2,2]     [3,2]
+[1,2]     [2,2]
+[0,2]     [1,2]
+[1,2]     [1,2]
+[2,2]     [1,2]
 
+
+Så här går det till:
+====================
+
+Först Ska vi ta fram dom koordinater som H kommer ta. Visualiserat ovan i vänster kolumnen. Dessa värden sparas i en lista.
+
+Vi skapar en tom T lista.
+
+Sen loopar vi igenom H Listan.
+
+För varje H Koordinat så kollar vi:
+
+X Diff.
+Y Diff.
+
+Sen jobbar vi med diffen mellan H och T.
+
+Diff = 0:0, T blir samma som innan
+Diff = 1:0 eller 0:1, T blir samma som innan
+Diff = 2:0 eller 0:2, så ska vi uppdatera T. Om vi ökar eller minska är beroende på positiv eller negativ diff nummber.
+Diff = 1:2 eller 2:1 så blir T vad H var steget innan.
+
+Vi avslutar med att pusha T in i T listan. Vi kommer behöva kolla vad senast värdet i den listan är när vi jämför.
+
+
+
+    
+. . . . .       . . . . .       . . . . .
+. . . . .       . . . . .       . . . . .
+. . . . .       . . . . H       . . . . H
+. . . . H       . . . . .       . . . . T
+. . . T .       . . . T .       . . . . .
+
+
+Det är nått när diffen är 1:2 mellan dom.....
+När diffen är 1:2 eller 2:1 så blir nästa T samma som H va steget innan.
+
+
+
+
+-------------------------------------
+R
+. . . . .   . . . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+. . H . .   . . . H .   . . T H .
+. T . . .   . T . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+2,2 - 1,1   3,2 - 1,1   3,2 - 2,2
+
+R
+. . . . .   . . . . .   . . . . .
+. T . . .   . T . . .   . . . . .
+. . H . .   . . . H .   . . T H .
+. . . . .   . . . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+
+U
+. . . . .   . . . . .   . . . . .
+. . . . .   . . H . .   . . H . .
+. . H . .   . . . . .   . . T . .
+. T . . .   . T . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+            
+D
+. . . . .   . . . . .   . . . . .
+. T . . .   . T . . .   . . . . .
+. . H . .   . . . . .   . . T . .
+. . . . .   . . H . .   . . H . .
+. . . . .   . . . . .   . . . . .
+
+---------------------------------------
+L
+. . . . .   . . . . .   . . . . .
+. . . T .   . . . T .   . . . . .
+. . H . .   . H . . .   . H T . .
+. . . . .   . . . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+
+L
+. . . . .   . . . . .   . . . . .
+. . . . .   . . . . .   . . . . .
+. . H . .   . H . . .   . H T . .
+. . . T .   . . . T .   . . . . .
+. . . . .   . . . . .   . . . . .
+
+D
+. . . . .   . . . . .   . . . . .
+. . . T .   . . . T .   . . . . .
+. . H . .   . . . . .   . . T . .
+. . . . .   . . H . .   . . H . .
+. . . . .   . . . . .   . . . . .
+
+U
+. . . . .   . . . . .   . . . . .
+. . . . .   . . H . .   . . H . .
+. . H . .   . . . . .   . . T . .
+. . . T .   . . . T .   . . . . .
+. . . . .   . . . . .   . . . . .
+-------------------------------------
+
+
+[0,0]                   [0,0]
+[1,0]   xd: +1 yd: +0     [0,0]
+[2,0]   xd: +2 yd: +0     [1,0]  X+ = TX+
+[3,0]   xd: +2 yd: +0     [2,0]  X+ = TX+
+[4,0]   xd: +2 yd: +0     [3,0]
+[4,1]   xd: +1 yd: +1     [3,0]
+[4,2]   xd: +1 yd: +2     [4,1] diff 1:2, T blir samma som H steget innan
+[4,3]   xd: +0 yd: +2     [4,2]
+[4,4]   xd: +0 yd: +2     [4,3]
+[3,4]   xd: -1 yd: +1     [4,3]
+[2,4]   xd: -2 yd: +1     [3,4] diff 2:1, T blir samma som H steget innan
+[1,4]   xd: -2 yd: +0     [2,4]
+[1,3]   xd: -1 yd: -1     [2,4]
+[2,3]   xd: +0 yd: -1     [2,4]
+[3,3]   xd: +1 yd: -1     [2,4]
+[4,3]   xd: +2 yd: -1     [3,3] diff 2:1, T blir samma som H steget innan
+[5,3]   xd: +2 yd: +0     [4,3]
+[5,2]   xd: +1 yd: -1     [4,3]
+[4,2]   xd: +0 yd: -1     [4,3]
+[3,2]   xd: -1 yd: -1     [4,3]
+[2,2]   xd: -2 yd: -1     [3,2] diff 2:1, T blir samma som H steget innan
+[1,2]   xd: -2 yd: +0     [2,2]
+[0,2]   xd: -2 yd: +0     [1,2]
+[1,2]   xd: +0 yd: +0     [1,2]
+[2,2]   xd: +1 yd: +0     [1,2]
+
+
+
+[0,0]   [0,0]
+
+[1,0]   [0,0]
+[2,0]   [1,0]
+[3,0]   [2,0]
+[4,0]   [3,0]
+[4,1]   [3,0]
+[4,2]   [4,1]
+[4,3]   [4,2]
+[4,4]   [4,3]
+[3,4]   [4,3]
+[2,4]   [3,4]
+[1,4]   [2,4]
+[1,3]   [2,4]
+[2,3]   [2,4]
+[3,3]   [2,4]
+[4,3]   [3,3]
+[5,3]   [4,3]
+[5,2]   [4,3]
+[4,2]   [4,3]
+[3,2]   [4,3]
+[2,2]   [3,2]
+[1,2]   [2,2]
+[0,2]   [1,2]
+[1,2]   [1,2]
+[2,2]   [1,2]
+
+[0,0]   [0,0]
+[1,0]   [1,0]
+[2,0]   [2,0]
+[3,0]   [3,0]
+[4,0]   [4,0]
+[4,1]   [4,1]
+[4,2]   [4,2]
+[4,3]   [4,3]
+[4,4]   [4,4]
+[3,4]   [3,4]
+[2,4]   [2,4]
+[1,4]   [1,4]
+[1,3]   [1,3]
+[2,3]   [2,3]
+[3,3]   [3,3]
+[4,3]   [4,3]
+[5,3]   [5,3]
+[5,2]   [5,2]
+[4,2]   [4,2]
+[3,2]   [3,2]
+[2,2]   [2,2]
+[1,2]   [1,2]
+[0,2]   [0,2]
+[1,2]   [1,2]
+[2,2]   [2,2]
 */
 
 type Move = [string, number];
 
+// const day9TestInput = `R 4
+// D 4
+// L 4
+// U 4`;
 const day9TestInput = `R 4
 U 4
 L 3
@@ -198,49 +383,81 @@ const movements = (input: string): Move[] =>
 const gameMovements = movements(day9TestInput);
 console.log('gameMovements:', gameMovements);
 
-const tailPositions: [number, number][] = [[0, 0]];
-const headPositions: [number, number][] = [[0, 0]];
+const getHeadCoords = (gameMovements: Move[]): number[][] => {
+    const headPositions = [[0, 0]];
 
-gameMovements.forEach((move) => {
-    const direction = move[0];
-    const steps = move[1];
+    gameMovements.forEach((move) => {
+        const direction = move[0];
+        const steps = move[1];
 
-    const lastTailPosition = tailPositions.at(-1);
-    const lastTailPositionX = lastTailPosition[0];
-    const lastTailPositionY = lastTailPosition[1];
+        const lastHeadPosition = headPositions.at(-1);
+        const lastHeadPositionX = lastHeadPosition[0];
+        const lastHeadPositionY = lastHeadPosition[1];
 
-    const lastHeadPosition = headPositions.at(-1);
-    const lastHeadPositionX = lastHeadPosition[0];
-    const lastHeadPositionY = lastHeadPosition[1];
+        const tempPos = [];
 
-    switch (direction) {
-        case 'R': {
-            // +X direction
-            console.log('Right');
+        if (direction === 'R') {
             let i = 0;
+
             while (i < steps) {
-                const nextPos = i + lastHeadPositionX + 1;
-                console.log('nextPos:', nextPos);
+                const lastTempPos = tempPos.at(-1);
+                if (!lastTempPos) {
+                    tempPos.push([lastHeadPositionX + 1, lastHeadPositionY]);
+                    i++;
+                    continue;
+                }
+
+                tempPos.push([lastTempPos[0] + 1, lastTempPos[1]]);
                 i++;
             }
-            return;
-        }
-        case 'L': {
-            console.log('Left');
-            return;
-        }
-        case 'U': {
-            console.log('Up');
-            return;
-        }
-        case 'D': {
-            console.log('Down');
-            return;
+        } else if (direction === 'U') {
+            let i = 0;
+
+            while (i < steps) {
+                const lastTempPos = tempPos.at(-1);
+                if (!lastTempPos) {
+                    tempPos.push([lastHeadPositionX, lastHeadPositionY + 1]);
+                    i++;
+                    continue;
+                }
+
+                tempPos.push([lastTempPos[0], lastTempPos[1] + 1]);
+                i++;
+            }
+        } else if (direction === 'L') {
+            let i = 0;
+
+            while (i < steps) {
+                const lastTempPos = tempPos.at(-1);
+                if (!lastTempPos) {
+                    tempPos.push([lastHeadPositionX - 1, lastHeadPositionY]);
+                    i++;
+                    continue;
+                }
+
+                tempPos.push([lastTempPos[0] - 1, lastTempPos[1]]);
+                i++;
+            }
+        } else if (direction === 'D') {
+            let i = 0;
+
+            while (i < steps) {
+                const lastTempPos = tempPos.at(-1);
+                if (!lastTempPos) {
+                    tempPos.push([lastHeadPositionX, lastHeadPositionY - 1]);
+                    i++;
+                    continue;
+                }
+
+                tempPos.push([lastTempPos[0], lastTempPos[1] - 1]);
+                i++;
+            }
         }
 
-        default: {
-            console.log('D');
-            return;
-        }
-    }
-});
+        headPositions.push(...tempPos);
+    });
+
+    return headPositions;
+};
+
+getHeadCoords(gameMovements);
